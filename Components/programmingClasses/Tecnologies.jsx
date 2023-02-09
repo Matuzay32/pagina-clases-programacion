73f0a730af6c6c0ea9fde15fd987f9a4b1136205
Matuzay32 <eze.rey92@gmail.com>
2023-02-09 20:36:51 -0300

    tengo que hacer el carru de tecnologias

---
+++ Components/programmingClasses/Tecnologies.jsx
@@ -0,0 +1,19 @@
+import {
+	Flex,
+	Box,
+	Text,
+	Stack,
+	Image,
+	chakra,
+	SimpleGrid,
+	HStack,
+} from "@chakra-ui/react";
+import React from "react";
+
+export default function Tecnologies() {
+	return (
+		<HStack justify={"center"}>
+			<Text>ACA VAN A IR LAS TECNOLOGIAS</Text>
+		</HStack>
+	);
+}
--- pages/programmingClasses.js
+++ pages/programmingClasses.js
@@ -2,11 +2,14 @@ import Contenedor from "../Components/paginaInicial/Contenedor";
 import Card from "../Components/programmingClasses/Card";
 import Pricing from "../Components/programmingClasses/Pricing";
 import ProcesoPrice from "../Components/programmingClasses/ProcesoPrice";
+import Tecnologies from "../Components/programmingClasses/Tecnologies";
 
 export default function thesisClasses() {
 	return (
 		<Contenedor>
 			<Card></Card>
+
+			<Tecnologies></Tecnologies>
 			<ProcesoPrice></ProcesoPrice>
 			<Pricing></Pricing>
 		</Contenedor>
